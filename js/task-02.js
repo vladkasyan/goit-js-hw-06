81ee504e3ceff2ca80ac861a3edc31956acf398e
vladkasyan <voadkasyan.2006@gmail.com>
2023-06-16 23:41:37 +0300

    1-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+ingredients.map((ingredient) => {
+  const itemEl = document.createElement(`li`);
+  itemEl.classList.add(`item`);
+  itemEl.textContent = ingredient;
+  return ulEl.append(itemEl)
+})
+
+console.log(ulEl)
