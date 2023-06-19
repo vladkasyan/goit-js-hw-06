da5f486f91f6c9fbb5f89afa73b911d2956f2e82
vladkasyan <voadkasyan.2006@gmail.com>
2023-06-19 23:37:26 +0300

    FIX

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,5 @@ const itemEl = document.querySelectorAll('.item')
 console.log("Number of categories:", itemEl.length)
 
 itemEl.forEach(item =>{
-    console.log("Category:", item.children[0].textContent)
-    console.log("Elements:", item.children[1].children.length)})
+    console.log("Category:", item.firstElementChild.textContent)
+    console.log("Elements:", item.lastElementChild.children.length)})
--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,8 @@ const span = document.querySelector('#name-output')
 
 const Eventlisten = Event => {
     span.textContent = Event.currentTarget.value;
-    if(Event.currentTarget.value === "false"){
-        return span.textContent = Anonymous;
+    if(!Event.currentTarget.value){
+        span.textContent = 'Anonymous';
     }
 }
 input.addEventListener("input", Eventlisten)
